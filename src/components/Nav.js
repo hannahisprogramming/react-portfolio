e18458d409fc75e74b3f30422974d648cf0e6805
Hannah Bush <heb.rutgers@gmail.com>
2022-06-06 17:14:18 -0400

    add css to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,35 +2,35 @@ import React from 'react';
 
 function Nav({ currentPage, handlePageChange}) {
   return (
-    <nav>
-      <div>
-        <a href='#about' onClick={() => handlePageChange('About')}>
+    <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+      <div className='container-fluid'>
+        <a  className='navbar-brand' href='#about' onClick={() => handlePageChange('About')}>
           Hannah's Portfolio
         </a>
-        <div id="navbar">
-          <ul>
-            <li>
+        <div id="navbar" className='collapse navbar-collapse'>
+          <ul className='navbar-nav ms-auto'>
+            <li className='nav-item'>
               <a href='#about' onClick={() => handlePageChange('About')}
               className={currentPage === 'About'}
               >
                 About Me
               </a>
             </li>
-            <li>
+            <li className='nav-item'>
               <a href='#projects' onClick={() => handlePageChange('Projects')}
               className={currentPage === 'Projects'}
               >
                 My Projects
               </a>
             </li>
-            <li>
+            <li className='nav-item'>
               <a href='#resume' onClick={() => handlePageChange('Resume')}
               className={currentPage === 'Resume'}
               >
                 My Resume
               </a>
             </li>
-            <li>
+            <li className='nav-item'>
               <a href='#contact' onClick={() => handlePageChange('Contact')}
               className={currentPage === 'Contact'}
               >
