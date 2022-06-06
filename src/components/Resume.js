ddbfaeb015faf053d700433419938748ae06a38a
Hannah Bush <heb.rutgers@gmail.com>
2022-06-06 19:48:59 -0400

    resume component coded

---
+++ src/components/Resume.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Resume () {
+  return (
+    <article className='pdf'>
+      <div>
+        <h2 id="resume" className='title'>Resume</h2>
+        <iframe
+          src="../assets/hannah-bush-resume.pdf" alt="Resume"
+          style={{
+            border: 0
+          }}
+          allowFullScreen
+          loading="lazy"
+        ></iframe>
+      </div>
+    </article>
+  );
+}
+
+export default Resume;
