3c8f8f3dad7e94709f9af9055e2d98c1ae9c73bd
Hannah Bush <heb.rutgers@gmail.com>
2022-06-06 16:58:37 -0400

    about coded

---
+++ src/components/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+//hero image
+
+function About() {
+  return (
+    <section className="about">
+      <h2>About Me</h2>
+      <p>
+        Welcome to my portfolio, my name is Hannah Bush. I've recently begun my
+        journey into being a software developer, starting by earning a Responsive
+        Web Design certificate from FreeCodeCamp in July of 2021, to now successfully 
+        graduating from Rutgers Coding Bootcamp and earning my certificate in Full 
+        Stack Web Development. I've completed over 30 projects, starting with small, 
+        strictly CSS and HTML webpages to a full MERN stack application. I can't wait 
+        to build some fun personal projects in the future-- plus my friends have 
+        already started making requests! 
+        My future goals for my career are to learn python and help in the development 
+        of automated robots!
+      </p>
+    </section>
+  );
+}
+
+export default About;
