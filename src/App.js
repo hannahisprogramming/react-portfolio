c19c64983462591666dc33a8671583510a9bde63
Hannah Bush <heb.rutgers@gmail.com>
2022-06-06 15:55:00 -0400

    app updated for file structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  
+  const [currentPage, setCurrentPage] = useState('About');
+
+  const renderPage = () => {
+    if(currentPage === 'About') {
+      return <About />;
+    }
+    if(currentPage === 'Projects') {
+      return <Projects />
+    }
+    if(currentPage === 'Resume') {
+      return <Resume />
+    }
+
+    return <Contact />
+  }
+
+  const handlePagechange = (page) => setCurrentPage(page);
   return (
+    <div>
+      <Nav currentPage={currentPage} handlePagechange={handlePagechange} />
+      {renderPage()}
+    </div>
   );
 }
 
